71c57ff7e7836e92229ff1d6d19a42a0906dd38e
IshmamD <ishmam2001@outlook.com>
2024-11-08 13:25:25 -0500

    Update IRDP2.py
    Spent a bunch of time figuring out why the imports werent working

--- IRDP2.py
+++ IRDP2.py
@@ -11,9 +11,13 @@ import numpy as np
 import pandas as pd
 from keras import layers
 from keras import models
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 #STEP 1
 
+input_shape = (500,500,3)
 
+train_dir = './P2D/Data/train'
+val_dir = './P2D/Data/valid'
+test_dir = './P2D/Data/test'
